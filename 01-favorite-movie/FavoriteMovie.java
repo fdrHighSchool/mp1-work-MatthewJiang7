cd2d776cbdee8ff339f2a2ed610c0d00cdcc52df
MatthewJiang7 <114923744+MatthewJiang7@users.noreply.github.com>
2022-10-04 08:20:35 -0400

    Add files via upload

---
+++ 01-favorite-movie/FavoriteMovie.java
@@ -0,0 +1,17 @@
+
+/**
+ * Describing favorite movie
+ *
+ * @author Matthew Jiang
+ * @version 09/21/22
+ */
+public class FavoriteMovie
+{
+    public static void main(String[] args) {
+        System.out.println("I don't have a favorite movie");
+        System.out.print("I don't watch any movie");
+        System.out.print(", it's not my form of entertainment. ");
+        System.out.println("I usually just use Youtube when bored");
+        System.out.println("Only rarely do I get to watch movies");
+        } //end of main
+} //end of class
